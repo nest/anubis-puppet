75719a0a836c9c4b52b635af28431459f3d50e4e
Yury V. Zaytsev <yury@shurup.com>
2011-07-01 19:38:54 +0200

    Anubis yum repos management configuration
    First, all files have to belong to root:root and have 644 permissions.
    
    Second, if puppet applies a configuration change, it has to re-generate the
    metadata. In the current scenario, when files are moved into the repo with the
    user / group that uploaded them, it makes sense. A better way possibly needs to
    be found.

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -4,6 +4,10 @@ node 'puppet.qa.nest-initiative.org' {
 
     include sudoers
 
+    # Local yum repositories on anubis (puppet etc.)
+    #
+    # TODO: think of how to best get the files there
+    #
     yumrepo { "rhel-6-local-noarch":
         baseurl => "file:///srv/repos/rhel-6-local/noarch",
         descr => "rhel-6-local-noarch",
@@ -16,6 +20,30 @@ node 'puppet.qa.nest-initiative.org' {
         descr => "rhel-6-local-x86_64",
         enabled => 1,
         gpgcheck => 0,
+    }
+
+    file { "/srv/repos":
+        ensure => "directory",
+        group => "root",
+        mode => 644,
+        owner => "root",
+        recurse => true,
+    }
+
+    file { "/srv/repos/update-metadata":
+        ensure => "file",
+        group => "root",
+        mode => 755,
+        owner => "root",
+        source => "puppet:///nodes/update-metadata",
+    }
+
+    exec { "update_metadata":
+        command => "/srv/repos/update-metadata",
+        cwd => "/srv/repos",
+        logoutput => true,
+        refreshonly => true,
+        subscribe => File["/srv/repos"],
     }
 
 }
--- manifests/site.pp
+++ manifests/site.pp
@@ -3,3 +3,9 @@
 import "classes.pp"
 import "nodes.pp"
 
+# Set the default execution path
+#
+Exec {
+    path => [ "/usr/local/bin", "/bin", "/usr/bin", "/usr/local/sbin", "/usr/sbin", "/sbin", ],
+}
+
