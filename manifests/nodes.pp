4ce215b8688fc89e487554aeb9b07021a905ce69
Yury V. Zaytsev <yury@shurup.com>
2011-07-01 18:46:56 +0200

    Local yum repos definitions for anubis

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -4,5 +4,19 @@ node 'puppet.qa.nest-initiative.org' {
 
     include sudoers
 
+    yumrepo { "rhel-6-local-noarch":
+        baseurl => "file:///srv/repos/rhel-6-local/noarch",
+        descr => "rhel-6-local-noarch",
+        enabled => 1,
+        gpgcheck => 0,
+    }
+
+    yumrepo { "rhel-6-local-x86_64":
+        baseurl => "file:///srv/repos/rhel-6-local/x86_64",
+        descr => "rhel-6-local-x86_64",
+        enabled => 1,
+        gpgcheck => 0,
+    }
+
 }
 
