7c6bc2303dbf812ac4411ee0fc53d94ba1a52717
Yury V. Zaytsev <yury@shurup.com>
2011-07-15 21:06:14 +0200

    Fix default action in the case statement: fail the manifest

--- manifests/classes.pp
+++ manifests/classes.pp
@@ -274,7 +274,7 @@ class ssh_server {
     case "$operatingsystem" {
         /RedHat|Fedora/: { $ssh_packages = [ 'openssh', 'openssh-server', 'openssh-clients', ] }
         /Debian|Ubuntu/: { $ssh_packages = [ 'openssh-server', 'openssh-client', ] }
-        default: undef
+        default: { fail("Unsupported operating system") }
     }
 
     package { $ssh_packages:
