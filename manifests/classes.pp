5b190e1a8e4a20a9d89ca075808f7decab8bcf28
Yury V. Zaytsev <yury@shurup.com>
2011-07-15 21:18:33 +0200

    Fix dependency on a list of packages

--- manifests/classes.pp
+++ manifests/classes.pp
@@ -278,19 +278,21 @@ class ssh_server {
     }
 
     package { $ssh_packages:
-        alias => 'openssh',
         ensure => 'present',
     }
 
     service { 'sshd':
+
         name => "${operatingsystem}" ? {
             /RedHat|Fedora/ => 'sshd',
             /Debian|Ubuntu/ => 'ssh',
             default => undef,
         },
+
         enable => 'true',
         ensure => 'running',
         require => Augeas['sshd_config'],
+
     }
 
     insert_comment { 'sshd_config':
@@ -298,13 +300,17 @@ class ssh_server {
     }
 
     augeas { 'sshd_config':
+
         context => '/files/etc/ssh/sshd_config',
+
         changes => [
             'set PasswordAuthentication "no"',
             'set GSSAPIAuthentication "no"',
         ],
+
         notify => Service['sshd'],
-        require => Package['openssh'],
+        require => Package[$ssh_packages],
+
     }
 
 }
