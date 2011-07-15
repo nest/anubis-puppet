70112cc223e5fe0f61415b70933898b89243e81b
Yury V. Zaytsev <yury@shurup.com>
2011-07-15 11:22:31 +0200

    Enable mail_server on puppet.qa.nest-initiative.org

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -7,9 +7,10 @@ node 'puppet.qa.nest-initiative.org' {
 
     include efi_backup
     include fstab
+    include mail_server
+    include master_configuration
     include rc_local
     include yum_repos_anubis
-    include master_configuration
 
     include site_ops
     include sudoers
