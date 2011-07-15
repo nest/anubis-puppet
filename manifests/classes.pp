eb82be9a45ec4f1fdd1c00f6389bde21356aab70
Yury V. Zaytsev <yury@shurup.com>
2011-07-15 13:47:08 +0200

    Add a define to tag configs managed by Puppet / Augeas and tag Postfix config

--- manifests/classes.pp
+++ manifests/classes.pp
@@ -1,6 +1,26 @@
 # ZYV
 
 #
+# Use Augeas to insert a comment tag at the top of the file, if it does not
+# already exist
+#
+# Most useful to indicate that the file is managed by Puppet
+#
+define insert_comment($file, $comment = 'ZYV: Managed by Puppet', $lens_comment = '#comment', $load_path = undef) {
+
+    augeas { "${file}: comment tag":
+        context => "/files${file}",
+        changes => [
+            "ins ${lens_comment} before *[1]",
+            "set ${lens_comment}[1] '${comment}'",
+        ],
+        onlyif => "match ${lens_comment}[.='${comment}'] size == 0",
+        load_path => $load_path,
+    }
+
+}
+
+#
 # Ensure that site admins are on the sudoers list
 #
 class sudoers {
@@ -117,6 +137,10 @@ class mail_server {
             Augeas['main.cf'],
             Package['postfix'],
         ]
+    }
+
+    insert_comment { 'main.cf':
+        file => '/etc/postfix/main.cf',
     }
 
     augeas { 'main.cf':
