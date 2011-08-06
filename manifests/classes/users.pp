f33b4734703e451c282bc0f7add5cc7aee4285aa
Yury V. Zaytsev <yury@shurup.com>
2011-08-06 23:53:41 +0200

    users: the routine to make users is more generic than it's name, fix it!

--- manifests/classes/users.pp
+++ manifests/classes/users.pp
@@ -18,51 +18,53 @@ class users::sudoers {
 
 }
 
-define users::make_admin($user_name, $user_id, $ssh_key = 'undefined', $ensure = 'present') {
+define users::make_user($user_name, $user_id, $groups = undef, $ssh_key = 'undefined', $ensure = 'present') {
 
     if $ensure == 'absent' {
         Ssh_authorized_key[$user_name] -> User[$user_name] -> Group[$user_name]
     }
 
-    user { "${user_name}":
+    user { $user_name:
         comment => 'Puppet-managed admin account',
-        ensure => "${ensure}",
-        gid => "${user_id}",
-        groups => [
-            'wheel',
-        ],
+        ensure => $ensure,
+        gid => $user_id,
+        groups => $groups,
         home => "/home/${user_name}",
         managehome => 'true',
         shell => '/bin/bash',
-        uid => "${user_id}",
+        uid => $user_id,
     }
 
-    group { "${user_name}":
-        ensure => "${ensure}",
-        gid => "${user_id}",
+    group { $user_name:
+        ensure => $ensure,
+        gid => $user_id,
         system => 'false',
     }
 
-    ssh_authorized_key { "${user_name}":
-        ensure => "${ensure}",
-        key => "${ssh_key}",
+    ssh_authorized_key { $user_name:
+        ensure => $ensure,
+        key => $ssh_key,
         type => 'ssh-rsa',
-        user => "${user_name}",
+        user => $user_name,
     }
 
 }
 
 class users::admins {
 
-    users::make_admin { 'zaytsev':
+    $admin_groups = [ 'wheel', ]
+
+    users::make_user { 'zaytsev':
         user_name => 'zaytsev',
         user_id => '501',
+        groups => $admin_groups,
         ssh_key => 'AAAAB3NzaC1yc2EAAAABIwAAAgEApS91Y4DxkPTFX6izRa/ClYc0qhNHsZvybAipbZPXbjdR0CRTgR4ZIzCzgdGh5iZexP9Q4ULHeR9ozrnXg69xmOdAJqq2/A0XGAvJq4sj+W38nDZ5tgT/8So2kH1ifjwT3ItF16aHi1b6GenMh2cB9mXu3VTE0rvsoTNoykbrT+GjlVjBT1UbfcUAbIR8Lz6LA+xUApIuz+eSWbyLMsjZoO0B8NO9boAVqVsw8CzwSZQZET0b6ekhde0YoQky7areuGvhtwQZ3XADWhPmkVmK8bBAffgdTOy6czHugfeq1NrFYUL0hEvV62a3uQnyDlBZNU8raGKEA8dCMN1uTdv1DVxTabxdd5fSTpRxbizp60XFj/DrU1JzIQ4w5zNlyM9yK3s+YD/pCCfz/iySaKp6VMEOkZNY0WrvfuiEcNSkh3Ga+5Nx0Q3m3SN06irSX8UYuy7y7Z23YpSDqdxnUm4ibQ2x+RGbv3CY5YuVyHbAe930T/94MMg3/h47YILXsnawA1OeyYF7UgydEOihC/FzkQf41ejvOmqPmSDHDRJRbeP97yH6O9pXvkuJavRY5/897HZab+MnCLXeWAAHhJ5UvcQq0Te+S8FwQqp9q4mDNzqjpC0549kck8qts1lHAzKfK/LrZV4nD+Pc/8rENrnOqpodkvNjOLjPu5BYYjporK0=',
     }
 
-    users::make_admin { 'wiebelt':
+    users::make_user { 'wiebelt':
         user_name => 'wiebelt',
         user_id => '502',
+        groups => $admin_groups,
         ensure => 'absent',
     }
 
